cb93a5a2464fe2455fde938fbfac04a93bfabbfb
Jason Hillinger <57076835+jasonhillinger@users.noreply.github.com>
2021-12-10 12:09:40 -0500

    testing static use of pokemon

---
+++ AllPokemon.java
@@ -0,0 +1,5 @@
+public class AllPokemon {
+    // All Pokemon are created here. This serves sort of as a database
+    public static Pokemon charmander = new Pokemon(120, "Charmander", 5, TYPE.FIRE);
+    public static Pokemon squirtle = new Pokemon(50, "Squirtle", 5, TYPE.WATER);
+}
--- Main.java
+++ Main.java
@@ -1,16 +1,18 @@
 public class Main {
     public static void main(String[] args) {
         Moves ember = new Moves("Ember", 4, 10, TYPE.FIRE, CONDITION.BURNING, 0);
-        Pokemon charmander = new Pokemon(120, "Charmander", 5, TYPE.FIRE);
-        Pokemon squirtle = new Pokemon(50, "Waffles", 5, TYPE.WATER);
+        
         Trainer trainer1 = new Trainer("Jason");
+        Trainer trainer2 = new Trainer("Hillinger");
 
-        trainer1.addPokemon(charmander);
+        trainer1.addPokemon(AllPokemon.charmander);
+        trainer2.addPokemon(AllPokemon.squirtle);
         // trainer1.getPokemon(0).attack(enemy, move);
-        charmander.setAbility(ember, 0);
-        charmander.printAbilities();
-        charmander.attack(squirtle, 0);
-        System.out.println(squirtle.getHealth());
+        trainer1.getPokemon(0).setAbility(ember, 0);
+        trainer1.getPokemon(0).printAbilities();
+        trainer1.getPokemon(0).attack(trainer2.getPokemon(0), 0);
+        System.out.println("STATIC : "+ AllPokemon.squirtle.getHealth());
+        System.out.println("TRAINER2 :" + trainer2.getPokemon(0).getHealth());
 
     }
 }
